e7227e8baf2e7c1d7782a3f8f54e17953816b880
Luis Choque <luighi0621@gmail.com>
2018-03-29 19:42:13 -0400

    adding repository

---
+++ StudentSolution/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentSolution.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSolution.Repository
+{
+    public interface IRepository<T>
+    {
+        void Save(T obj);
+        void Remove(T obj);
+        List<T> GetList();
+    }
+}
---
+++ StudentSolution/Repository/StudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using StudentSolution.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSolution.Repository
+{
+    class StudentRepository : IRepository<Student>
+    {
+        public List<Student> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Student obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Student obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
