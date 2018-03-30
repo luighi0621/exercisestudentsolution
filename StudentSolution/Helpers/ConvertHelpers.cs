86925bcd644f7b2ba4ddd68f59ab50b7f2bc7779
Luis Choque <luighi0621@gmail.com>
2018-03-29 20:33:55 -0400

    creating helpers

---
+++ StudentSolution/Helpers/ConvertHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static StudentSolution.Helpers.Enums;
+
+namespace StudentSolution.Helpers
+{
+    public static class ConvertHelpers
+    {
+        public static char ConvertEnumGenderToChar(Gender gender)
+        {
+            if (gender == Gender.Male)
+            {
+                return 'M';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
+        public static string ConvertToTimeStamp(DateTime date)
+        {
+            return date.ToString("yyyyMMddHHmmss");
+        }
+    }
+}
--- StudentSolution/Model/Student.cs
+++ StudentSolution/Model/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentSolution.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,21 @@ namespace StudentSolution.Model
 {
     public class Student
     {
-        public string Name { get; set; }
-        public Gender Gender { get; set; }
-        public StudentType StudentType { get; set; }
-        public DateTime LastUpdate { get; set; }
+        public string Name { get; private set; }
+        public Gender Gender { get; private set; }
+        public StudentType StudentType { get; private set; }
+        public DateTime LastUpdate { get; private set; }
 
         public Student(string cvsLine)
         {
             var values = cvsLine.Split(',');
             StudentType = (StudentType)Enum.Parse(typeof(StudentType), values[0], true);
             Name = values[1];
-        
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3}",StudentType.ToString(), Name, ConvertHelpers.ConvertEnumGenderToChar(Gender), ConvertHelpers.ConvertToTimeStamp(LastUpdate));
+        }
     }
 }
