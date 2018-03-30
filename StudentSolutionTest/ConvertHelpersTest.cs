0372e280d455a56b23b4b09624e221ec80b5f965
Luis Choque <luighi0621@gmail.com>
2018-03-29 21:28:23 -0400

    added test solution

---
+++ StudentSolutionTest/ConvertHelpersTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static StudentSolution.Helpers.Enums;
+using StudentSolution.Helpers;
+
+namespace StudentSolutionTest
+{
+    [TestClass]
+    public class ConvertHelpersTest
+    {
+        [TestMethod]
+        public void ReturnCharFromGender()
+        {
+            Gender male = Gender.Male;
+            char maleValue = ConvertHelpers.ConvertEnumGenderToChar(male);
+            Assert.AreEqual('M', maleValue);
+        }
+    }
+}
---
+++ StudentSolutionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StudentSolutionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("StudentSolutionTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8732172-445e-4b47-aebb-12833afd91b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
